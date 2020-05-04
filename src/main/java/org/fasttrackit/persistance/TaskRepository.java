dbf991395d916a8a369ebe73ddf17e6cfbf650eb
AncutaBiro <ancuta.biro@yahoo.com>
2020-05-04 21:45:15 +0300

    added request to database

---
+++ src/main/java/org/fasttrackit/persistance/TaskRepository.java
@@ -0,0 +1,30 @@
+package org.fasttrackit.persistance;
+
+import com.sun.org.apache.bcel.internal.generic.ArrayElementValueGen;
+import org.fasttrackit.transfer.CreateTaskRequest;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TaskRepository {
+
+    public void createTask(CreateTaskRequest request) throws IOException, SQLException {
+// preventing SQL injection by using PreparedStatement
+        String sql = "INSERT INTO task (description, deadline) VALUES (?,?)";
+
+        // try-with-resources
+        try (Connection connection =  DataBaseConfiguration.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+        preparedStatement.setString(1, request.getDescription());
+        preparedStatement.setDate(2, Date.valueOf(request.getDeadline()));
+
+        preparedStatement.executeUpdate();
+        }
+
+    }
+
+}
