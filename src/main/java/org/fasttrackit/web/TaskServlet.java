5fbd4e5c97cb6ffd1b1e952e667d2909626038ea
AncutaBiro <ancuta.biro@yahoo.com>
2020-05-25 20:32:28 +0300

    added TaskServlet - others methods

--- src/main/java/org/fasttrackit/web/TaskServlet.java
+++ src/main/java/org/fasttrackit/web/TaskServlet.java
@@ -2,8 +2,11 @@ package org.fasttrackit.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.fasttrackit.config.ObjectMapperConfiguration;
+import org.fasttrackit.domain.Task;
 import org.fasttrackit.service.TaskService;
 import org.fasttrackit.transfer.CreateTaskRequest;
+import org.fasttrackit.transfer.UpdateTaskRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/tasks")
 public class TaskServlet extends HttpServlet {
@@ -21,10 +25,9 @@ public class TaskServlet extends HttpServlet {
     // endpoint
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
 
-        CreateTaskRequest request = new ObjectMapper().readValue(req.getReader(), CreateTaskRequest.class);
+        CreateTaskRequest request = ObjectMapperConfiguration.OBJECT_MAPPER.
+                readValue(req.getReader(), CreateTaskRequest.class);
 
         try {
             taskService.createTask(request);
@@ -32,4 +35,44 @@ public class TaskServlet extends HttpServlet {
             resp.sendError(500, "There was an error while processing your request. " + e.getMessage());
         }
     }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        UpdateTaskRequest request = ObjectMapperConfiguration.OBJECT_MAPPER.
+                readValue(req.getReader(), UpdateTaskRequest.class);
+
+        try {
+            taskService.updateTask(Long.parseLong(id), request);
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "There was an error while processing your request. " + e.getMessage());
+        }
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        try {
+            taskService.deleteTask (Long.parseLong(id));
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "There was an error while processing your request. " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            List<Task> tasks = taskService.getTasks();
+
+            ObjectMapperConfiguration.OBJECT_MAPPER.
+                    writeValue(resp.getWriter(), tasks);
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "There was an error while processing your request. " + e.getMessage());
+        }
+
+    }
 }
+
